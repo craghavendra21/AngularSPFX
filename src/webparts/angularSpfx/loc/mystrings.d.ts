674c823207a665ab397b25261055445d9ad98a7b
Raghavendra <craghavendra21@gmail.com>
2019-01-31 15:58:05 +0530

    Angular SPFX Files Added

---
+++ src/webparts/angularSpfx/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IAngularSpfxWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'AngularSpfxWebPartStrings' {
+  const strings: IAngularSpfxWebPartStrings;
+  export = strings;
+}
